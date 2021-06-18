9cd94e2fc701bc5cda5eabf11349b2d25e6a6520
Mark Roth <mark_roth@byu.edu>
2021-06-17 22:30:29 -0600

    updated zip and handler to try catch

--- lambda/src/handler.py
+++ lambda/src/handler.py
@@ -16,40 +16,47 @@ def lambda_handler(event, context):
 
     print('Beginning Postman Tests')
 
-    postman_lambda_response = lambda_client.invoke(
-        FunctionName=os.environ['POSTMAN_LAMBDA_ARN'],
-        InvocationType='RequestResponse',
-        Payload=json.dumps(input_parameters)
-    )
-    postman_response = json.load(postman_lambda_response['Payload'])
-    if postman_response['passed']:
-        print('Postman tests passed. Beginning UI tests')
-    else:
+    try:
+        postman_lambda_response = lambda_client.invoke(
+            FunctionName=os.environ['POSTMAN_LAMBDA_ARN'],
+            InvocationType='RequestResponse',
+            Payload=json.dumps(input_parameters)
+        )
+        postman_response = json.load(postman_lambda_response['Payload'])
+        if postman_response['passed']:
+            print('Postman tests passed. Beginning UI tests')
+        else:
+            codedeploy_client.put_lifecycle_event_hook_execution_status(
+                deploymentId=deployment_id,
+                lifecycleEventHookExecutionId=lifecycle_event_hook_execution_id,
+                status='Failed'
+            )
+            raise Exception('Postman tests failed')
+
+        ui_lambda_response = lambda_client.invoke(
+            FunctionName=os.environ['UI_LAMBDA_ARN'],
+            InvocationType='RequestResponse',
+            Payload=json.dumps(input_parameters)
+        )
+        ui_response = json.load(ui_lambda_response['Payload'])
+        if ui_response['passed']:
+            print('UI tests successfully ran')
+        else:
+            codedeploy_client.put_lifecycle_event_hook_execution_status(
+                deploymentId=deployment_id,
+                lifecycleEventHookExecutionId=lifecycle_event_hook_execution_id,
+                status='Failed'
+            )
+            raise Exception('UI test failed')
+
+        codedeploy_client.put_lifecycle_event_hook_execution_status(
+            deploymentId=deployment_id,
+            lifecycleEventHookExecutionId=lifecycle_event_hook_execution_id,
+            status='Succeeded'
+        )
+    except:
         codedeploy_client.put_lifecycle_event_hook_execution_status(
             deploymentId=deployment_id,
             lifecycleEventHookExecutionId=lifecycle_event_hook_execution_id,
             status='Failed'
         )
-        raise Exception('Postman tests failed')
-
-    ui_lambda_response = lambda_client.invoke(
-        FunctionName=os.environ['UI_LAMBDA_ARN'],
-        InvocationType='RequestResponse',
-        Payload=json.dumps(input_parameters)
-    )
-    ui_response = json.load(ui_lambda_response['Payload'])
-    if ui_response['passed']:
-        print('UI tests successfully ran')
-    else:
-        codedeploy_client.put_lifecycle_event_hook_execution_status(
-            deploymentId=deployment_id,
-            lifecycleEventHookExecutionId=lifecycle_event_hook_execution_id,
-            status='Failed'
-        )
-        raise Exception('UI test failed')
-
-    codedeploy_client.put_lifecycle_event_hook_execution_status(
-        deploymentId=deployment_id,
-        lifecycleEventHookExecutionId=lifecycle_event_hook_execution_id,
-        status='Succeeded'
-    )
